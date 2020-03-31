f64c0a9a460bcac25df9050f1d535d2d9adc04be
SandraRadz <oleksandraradzievska@gmail.com>
2020-03-31 15:50:35 +0300

    language localization

--- app/src/main/java/com/radzievska/oleksandra/androidframework/ARModule/AugmentedImageFragment.java
+++ app/src/main/java/com/radzievska/oleksandra/androidframework/ARModule/AugmentedImageFragment.java
@@ -26,28 +26,17 @@ import java.io.InputStream;
 public class AugmentedImageFragment extends ArFragment {
     private static final String TAG = "AugmentedImageFragment";
 
-    // This is the name of the image in the sample database.  A copy of the image is in the assets
-    // directory.  Opening this image on your computer is a good quick way to test the augmented image
-    // matching.
     private static final String DEFAULT_IMAGE_NAME = "default.jpg";
-
-    // This is a pre-created database containing the sample image.
     private static final String SAMPLE_IMAGE_DATABASE = "sample_database.imgdb";
 
-    // Augmented image configuration and rendering.
     // Load a single image (true) or a pre-generated image database (false).
     private static final boolean USE_SINGLE_IMAGE = false;
-
-    // Do a runtime check for the OpenGL level available at runtime to avoid Sceneform crashing the
-    // application.
     private static final double MIN_OPENGL_VERSION = 3.0;
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
 
-        // Check for Sceneform being supported on this device.  This check will be integrated into
-        // Sceneform eventually.
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
             Log.e(TAG, "Sceneform requires Android N or later");
             SnackbarHelper.getInstance()
@@ -69,8 +58,6 @@ public class AugmentedImageFragment extends ArFragment {
     public View onCreateView(
             LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = super.onCreateView(inflater, container, savedInstanceState);
-
-        // Turn off the plane discovery since we're only looking for images
         getPlaneDiscoveryController().hide();
         getPlaneDiscoveryController().setInstructionView(null);
         getArSceneView().getPlaneRenderer().setEnabled(false);
@@ -101,27 +88,15 @@ public class AugmentedImageFragment extends ArFragment {
             return false;
         }
 
-        // There are two ways to configure an AugmentedImageDatabase:
-        // 1. Add Bitmap to DB directly
-        // 2. Load a pre-built AugmentedImageDatabase
-        // Option 2) has
-        // * shorter setup time
-        // * doesn't require images to be packaged in apk.
+
         if (USE_SINGLE_IMAGE) {
             Bitmap augmentedImageBitmap = loadAugmentedImageBitmap(assetManager);
             if (augmentedImageBitmap == null) {
                 return false;
             }
-
             augmentedImageDatabase = new AugmentedImageDatabase(session);
             augmentedImageDatabase.addImage(DEFAULT_IMAGE_NAME, augmentedImageBitmap);
-            // If the physical size of the image is known, you can instead use:
-            //     augmentedImageDatabase.addImage("image_name", augmentedImageBitmap, widthInMeters);
-            // This will improve the initial detection speed. ARCore will still actively estimate the
-            // physical size of the image as it is viewed from multiple viewpoints.
         } else {
-            // This is an alternative way to initialize an AugmentedImageDatabase instance,
-            // load a pre-existing augmented image database.
             try (InputStream is = getContext().getAssets().open(SAMPLE_IMAGE_DATABASE)) {
                 augmentedImageDatabase = AugmentedImageDatabase.deserialize(session, is);
             } catch (IOException e) {
@@ -129,7 +104,6 @@ public class AugmentedImageFragment extends ArFragment {
                 return false;
             }
         }
-
         config.setAugmentedImageDatabase(augmentedImageDatabase);
         return true;
     }
